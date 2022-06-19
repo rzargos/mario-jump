8b73a2f903739745b3dfe292140bb8d9943a4c7d
rzargos <raphael.zargos@gmail.com>
2022-06-19 16:29:10 -0300

    A Mario Game

--- js/script.js
+++ js/script.js
@@ -1,6 +1,42 @@
+// alert('Use the space bar for jump')
 const mario = document.querySelector('.mario')
+const pipe = document.querySelector('.pipe')
+const clouds = document.querySelector('.clouds')
+const gameOver = document.querySelector('.game-over')
+const gameBoard = document.querySelector('.game-board')
 
-const jump = () => {
-
+const jump = (event) => {
+    if (event.key == " ") {
+        mario.classList.add('jump')
+        setTimeout(() => {
+            mario.classList.remove('jump')
+        },500)
+    }
 }
-document.addEventListener('keydown', jump);
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','')
+    const cloudsPosition = clouds.offsetLeft
+
+    if (pipePosition <= 120 && pipePosition > 0  && marioPosition < 80) {
+
+        pipe.style.animation = 'none'
+        pipe.style.left = `${pipePosition}px`
+
+        mario.style.animation = 'none'
+        mario.style.bottom = `${marioPosition}px`
+
+        clouds.style.animation = 'none'
+        clouds.style.left = `${cloudsPosition}px`
+
+        mario.src = './images/game-over.png'
+        mario.style.width = '75px ' 
+        mario.style.marginLeft = '50px'
+
+        clearInterval(loop)
+    }
+}, 10)
+
+document.addEventListener('keydown', jump) 
