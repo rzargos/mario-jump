5239c6d0952e90f78359988f090ccfa1ebb5fb54
rzargos <raphael.zargos@gmail.com>
2022-06-21 16:09:23 -0300

    It was implemented a start and reset button

--- js/script.js
+++ js/script.js
@@ -1,42 +1,87 @@
 // alert('Use the space bar for jump')
+
 const mario = document.querySelector('.mario')
 const pipe = document.querySelector('.pipe')
 const clouds = document.querySelector('.clouds')
 const gameOver = document.querySelector('.game-over')
 const gameBoard = document.querySelector('.game-board')
+const startButton = document.querySelector('.start-button')
+
+let isPlaing = false
+
+pipe.style.animation = 'none'
+//mario.style.animation = 'none'
+clouds.style.animation = 'none'
 
 const jump = (event) => {
     if (event.key == " ") {
         mario.classList.add('jump')
+        
         setTimeout(() => {
             mario.classList.remove('jump')
         },500)
     }
 }
+function validation() {
+    const loop = setInterval(() => {
+    
+        const pipePosition = pipe.offsetLeft
+        const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','')
+        const cloudsPosition = clouds.offsetLeft
+    
+        if (pipePosition <= 120 && pipePosition > 0  && marioPosition < 80) {
+    
+            pipe.style.animation = 'none'
+            pipe.style.left = `${pipePosition}px`
+    
+            //mario.style.animation = 'none'
+            mario.classList.remove('jump')
+            mario.style.bottom = `${marioPosition}px`
+    
+            clouds.style.animation = 'none'
+            clouds.style.left = `${cloudsPosition}px`
+    
+            mario.src = './images/game-over.png'
+            mario.style.width = '75px ' 
+            mario.style.marginLeft = '50px'
 
-const loop = setInterval(() => {
+            startButton.removeAttribute('disabled')
+    
+            isPlaing = false
+    
+            clearInterval(loop)
+        }
+    }, 10)
+}
 
-    const pipePosition = pipe.offsetLeft
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','')
-    const cloudsPosition = clouds.offsetLeft
-
-    if (pipePosition <= 120 && pipePosition > 0  && marioPosition < 80) {
-
-        pipe.style.animation = 'none'
-        pipe.style.left = `${pipePosition}px`
-
-        mario.style.animation = 'none'
-        mario.style.bottom = `${marioPosition}px`
-
-        clouds.style.animation = 'none'
-        clouds.style.left = `${cloudsPosition}px`
-
-        mario.src = './images/game-over.png'
-        mario.style.width = '75px ' 
-        mario.style.marginLeft = '50px'
-
-        clearInterval(loop)
+document.addEventListener('keydown', (event) => {
+    console.log(isPlaing)
+    if (isPlaing) {
+        jump(event)
     }
-}, 10)
+}) 
 
-document.addEventListener('keydown', jump) 
+animation = {
+    mario: 'animation: jump 500ms ease-out',
+    pipe:  'pipe-animation 1.5s infinite linear',
+    clouds: 'clouds-animation 20s infinite linear'
+}
+
+function start() {
+    
+    isPlaing = true
+    validation()
+    pipe.style.animation = animation.pipe
+    //mario.style.animation = animation.mario
+    clouds.style.animation = animation.clouds
+
+    pipe.style.left = ``
+    mario.style.bottom = ``
+    clouds.style.left = ``
+
+    mario.src = './images/mario.gif'
+    mario.style.width = '150px' 
+    mario.style.marginLeft = ''
+
+    startButton.setAttribute('disabled', 'true')
+}
